fa3ca386d4efc257a86e2e829d28e95f09d626e7
Akshat Mehta <akshat2404@gmail.com>
2023-01-14 16:07:03 +0530

    NavBar and Background Styling

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,39 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoH from '../../assets/images/logo-harshith.png'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () =>(
+    <div className='nav-bar'>
+        <Link className='logo' to = '/'>
+            <img className='logoh' src={LogoH} alt="logo"/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/harshithdshetty/'>
+                    <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Harshith-Shetty'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
